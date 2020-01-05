04709413aa0ef52a8f948eaf4ec783a846b4344f
FamSlayer <fuller.rules@gmail.com>
2020-01-04 19:03:27 -0500

    eneko v skullex

--- match reader.py
+++ match reader.py
@@ -6,8 +6,8 @@ MOVES_THAT_POISON = ["Gunk Shot", "Poison Fang", "Poison Gas", "Poison Jab", "Po
 
 
 
-player_1 = "Poisseman"
-player_2 = "Tan"
+player_1 = "Eneko"
+player_2 = "Skullex"
 
 f = open(player_1 + " vs " + player_2 + ".txt",'r').read()
 ##f = open("status battle 2.txt",'r').read()
